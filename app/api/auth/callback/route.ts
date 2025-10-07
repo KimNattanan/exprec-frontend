d3dea68cca335a329173da314e39d96b94610352
KimNattanan <jeung.nattanan@gmail.com>
2025-10-07 23:56:57 +0700

    test

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -1,24 +1,22 @@
 import { NextResponse } from "next/server";
 
 export function GET(req: Request) {
-  return NextResponse.redirect(new URL("/", req.url));
+  const url = new URL(req.url);
+  const token = url.searchParams.get("token");
 
-  // const url = new URL(req.url);
-  // const token = url.searchParams.get("token");
+  if (!token) {
+    return NextResponse.redirect(new URL("/login", req.url)); // fallback if no token
+  }
 
-  // if (!token) {
-  //   return NextResponse.redirect(new URL("/login", req.url)); // fallback if no token
-  // }
+  const res = NextResponse.redirect(new URL("/", req.url));
 
-  // const res = NextResponse.redirect(new URL("/", req.url));
+  res.cookies.set("loginToken", token, {
+    httpOnly: true,
+    secure: true,    
+    sameSite: "lax",
+    path: "/",
+    maxAge: 60 * 60 * 24 * 3, // 3 days
+  });
 
-  // res.cookies.set("loginToken", token, {
-  //   httpOnly: true,
-  //   secure: true,    
-  //   sameSite: "lax",
-  //   path: "/",
-  //   maxAge: 60 * 60 * 24 * 3, // 3 days
-  // });
-
-  // return res;
+  return res;
 }
--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "https://9z0vlysskl.execute-api.ap-southeast-2.amazonaws.com/default/:path*",
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
