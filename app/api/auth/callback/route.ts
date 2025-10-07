79931dd67d4f481c3bfbfc965266a43fa6023c5c
KimNattanan <jeung.nattanan@gmail.com>
2025-10-07 23:54:39 +0700

    test

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -2,23 +2,23 @@ import { NextResponse } from "next/server";
 
 export function GET(req: Request) {
   return NextResponse.redirect(new URL("/", req.url));
-  
-  const url = new URL(req.url);
-  const token = url.searchParams.get("token");
 
-  if (!token) {
-    return NextResponse.redirect(new URL("/login", req.url)); // fallback if no token
-  }
+  // const url = new URL(req.url);
+  // const token = url.searchParams.get("token");
 
-  const res = NextResponse.redirect(new URL("/", req.url));
+  // if (!token) {
+  //   return NextResponse.redirect(new URL("/login", req.url)); // fallback if no token
+  // }
 
-  res.cookies.set("loginToken", token, {
-    httpOnly: true,
-    secure: true,    
-    sameSite: "lax",
-    path: "/",
-    maxAge: 60 * 60 * 24 * 3, // 3 days
-  });
+  // const res = NextResponse.redirect(new URL("/", req.url));
 
-  return res;
+  // res.cookies.set("loginToken", token, {
+  //   httpOnly: true,
+  //   secure: true,    
+  //   sameSite: "lax",
+  //   path: "/",
+  //   maxAge: 60 * 60 * 24 * 3, // 3 days
+  // });
+
+  // return res;
 }
