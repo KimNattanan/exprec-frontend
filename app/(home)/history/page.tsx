66bba01ebe4d1cca1ede5b86b6b23d663c2852ea
KimNattanan <jeung.nattanan@gmail.com>
2025-10-07 23:06:07 +0700

    fix Suspense in app/history

--- app/(home)/history/page.tsx
+++ app/(home)/history/page.tsx
@@ -10,25 +10,25 @@ import { Suspense } from "react";
 export default async function History(){
   return (
     <>
-      <HistoryContextProvider>
-        <HistoryEditModeBtn/>
-        <div className="text-foreground w-full min-h-dvh xs:px-10 px-4 mb-4">
-          <div className="
-            w-fit mx-auto 
-            font-medium text-bad
-            lg:text-8xl
-            md:text-7xl
-            sm:text-6xl sm:mb-10
-            xs:text-5xl xs:mb-6
-            text-4xl mt-10 mb-4
-          ">History</div>
-          <Suspense>
-            <HistoryPaginator/>
-            <RecordContainer/>
-            <HistoryPaginator/>
-          </Suspense>
+      <div className="text-foreground w-full min-h-dvh xs:px-10 px-4 mb-4">
+        <div className="
+          w-fit mx-auto 
+          font-medium text-bad
+          lg:text-8xl
+          md:text-7xl
+          sm:text-6xl sm:mb-10
+          xs:text-5xl xs:mb-6
+          text-4xl mt-10 mb-4
+        ">History</div>
+        <Suspense>
+          <HistoryContextProvider>
+            <HistoryEditModeBtn/>
+              <HistoryPaginator/>
+              <RecordContainer/>
+              <HistoryPaginator/>
+          </HistoryContextProvider>
+        </Suspense>
         </div>
-      </HistoryContextProvider>
       <Footer/>
     </>
   )
