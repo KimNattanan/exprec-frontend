3e6f295acd2c5b741c5d7cd86b7a0226c887dd26
KimNattanan <jeung.nattanan@gmail.com>
2025-10-08 09:10:01 +0700

    test cookie

---
+++ app/api/auth/test/fetch/route.ts
@@ -0,0 +1,14 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const BACKEND_URL = (process.env.NEXT_PUBLIC_BACKEND_URL || '').replace(/\/+$/,'')
+
+export async function GET(req: NextRequest) {
+  const res = await fetch(`${BACKEND_URL}/api/v2/test/fetch-cookie`, {
+    method: 'GET',
+    credentials: 'include'
+  })
+  if(!res.ok){
+    return NextResponse.json({ error: "fail" }, { status: 500 })
+  }
+  return NextResponse.json({ message: await res.text() }, { status: 200 });
+}
