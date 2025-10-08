af74ec9cc16e4c83182fb3eb5312e8c9023fed36
KimNattanan <jeung.nattanan@gmail.com>
2025-10-08 17:30:51 +0700

    ..

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
-import { importJWK, jwtVerify } from "jose";
+import { importJWK, jwtVerify, SignJWT } from "jose";
+import { cookies as _cookies } from "next/headers";
 
 export async function middleware(req: NextRequest) {
   try {
@@ -10,13 +11,27 @@ export async function middleware(req: NextRequest) {
       k: jwtSecret
     }
     const secretKey = await importJWK(secretJWK, 'HS256')
-    
-    let loginToken = req.cookies.get('login-token')?.value;
+
+    let loginToken = req.cookies.get('token')?.value;
     if(!loginToken){
       const authorizationHeader = req.headers.get('Authorization');
+      console.log("auth header: ", authorizationHeader)
       if (authorizationHeader && authorizationHeader.startsWith('Bearer ')) {
         loginToken = authorizationHeader.substring(7);
+        const secret = new TextEncoder().encode(jwtSecret);
+        const { payload } = await jwtVerify(loginToken, secret);
+        const userEmail = payload.email as string
+        
+        const token = await new SignJWT({ userEmail }).setProtectedHeader({ alg: 'HS256' })
+                                                  .setIssuedAt()
+                                                  .setExpirationTime('2352h')
+                                                  .sign(secretKey);
+        const cookies = await _cookies();
+        cookies.set("token", token);
+        loginToken = token
+        
       } else {
+        console.error("no header")
         throw new Error("Unauthorized");
       }
     }
