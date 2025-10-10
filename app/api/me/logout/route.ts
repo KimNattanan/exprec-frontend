976a120f350e0ddb0de3dc1e72108843df401dc2
KimNattanan <jeung.nattanan@gmail.com>
2025-10-10 16:46:51 +0700

    fix logout route

--- app/api/me/logout/route.ts
+++ app/api/me/logout/route.ts
@@ -2,11 +2,11 @@ import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
-  const userEmail = req.headers.get("x-user-email");
-  if (!userEmail) {
+  const user = req.headers.get("x-user");
+  if (!user) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
   const cookieStore = await cookies();
-  cookieStore.delete("login-token");
+  cookieStore.delete("loginToken");
   return NextResponse.json(null, { status: 200 });
 }
