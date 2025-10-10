4424dc5a4dc00b7d08d1c46b8f7db0e3ddd2c9f2
KimNattanan <jeung.nattanan@gmail.com>
2025-10-10 16:44:51 +0700

    fix middleware

--- app/api/me/route.ts
+++ app/api/me/route.ts
@@ -1,9 +1,11 @@
+import { User } from "@/types/api";
 import { NextRequest, NextResponse } from "next/server";
 
 export function GET(req: NextRequest) {
-  const email = req.headers.get("x-user-email");
-  if(!email){
+  const userStr = req.headers.get("x-user");
+  if(!userStr){
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
-  return NextResponse.json({email}, { status: 200 })
+  const user: User = JSON.parse(userStr);
+  return NextResponse.json(user, { status: 200 })
 }
--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,26 @@
 import { NextRequest, NextResponse } from "next/server";
 import { importJWK, jwtVerify, SignJWT } from "jose";
 import { cookies as _cookies } from "next/headers";
+import { User } from "./types/api";
 
 export async function middleware(req: NextRequest) {
   try {
+    const loginToken = req.cookies.get('loginToken');
+    if (!loginToken) throw new Error("loginToken not found");
+
     const jwtSecret = process.env.JWT_SECRET;
     if (!jwtSecret) throw new Error("JWT_SECRET not set");
-    const secretJWK = {
-      kty: 'oct',
-      k: jwtSecret
-    }
-    const secretKey = await importJWK(secretJWK, 'HS256')
-    const loginToken = req.cookies.get('login-token')?.value;
-    if(!loginToken) throw new Error("Unauthorized");
 
-    const { payload } = await jwtVerify(loginToken, secretKey);
-    const userEmail = payload.email as string
+    const secret = new TextEncoder().encode(jwtSecret);
+    const { payload } = await jwtVerify(loginToken.value, secret);
+    const user = payload.user as User
 
     const headers = new Headers(req.headers);
-    headers.set('x-user-email', userEmail);
-
+    headers.set('x-user', JSON.stringify(user));
+    
     return NextResponse.next({ request: { headers } });
   } catch(error) {
-    console.error(error);
+    console.log(error);
     return NextResponse.redirect(new URL('/login', req.url));
   }
 }
