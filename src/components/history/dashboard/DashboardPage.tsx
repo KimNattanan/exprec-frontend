f9d331936e50f3a3f7eb3f5c9105a019e23156bc
KimNattanan <jeung.nattanan@gmail.com>
2025-10-12 19:12:36 +0700

    add date input placeholder

--- src/components/history/dashboard/DashboardPage.tsx
+++ src/components/history/dashboard/DashboardPage.tsx
@@ -52,6 +52,7 @@ function SearchBox() {
         <div className="ml-4 mb-4">
           <input
             name="date-from"
+            placeholder="select date"
             type="date"
             value={dateFrom}
             onChange={(e: ChangeEvent<HTMLInputElement>)=>setDateFrom(e.target.value)}
@@ -64,6 +65,7 @@ function SearchBox() {
         <div className="ml-4 mb-4">
           <input
             name="date-to"
+            placeholder="select date"
             type="date"
             value={dateTo}
             onChange={(e: ChangeEvent<HTMLInputElement>)=>setDateTo(e.target.value)}
